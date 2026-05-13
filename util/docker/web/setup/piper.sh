f14fb3d66918a87f7f7fa958630e380170d7a904
NaNomicon <lecongnhan29@gmail.com>
2026-05-14 06:09:23 +0700

    build: install additional Piper voices
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- util/docker/web/setup/piper.sh
+++ util/docker/web/setup/piper.sh
@@ -21,11 +21,19 @@ tar -xzf /tmp/piper.tar.gz -C /usr/local/share/piper/ --strip-components=1
 ln -sf /usr/local/share/piper/piper /usr/local/bin/piper
 chmod a+x /usr/local/share/piper/piper
 
-# Install default voice model (en_US-lessac-medium)
-mkdir -p /usr/local/share/piper-voices/en/en_US/lessac/medium
-wget -O /usr/local/share/piper-voices/en/en_US/lessac/medium/en_US-lessac-medium.onnx \
-  "https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/lessac/medium/en_US-lessac-medium.onnx"
-wget -O /usr/local/share/piper-voices/en/en_US/lessac/medium/en_US-lessac-medium.onnx.json \
-  "https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/lessac/medium/en_US-lessac-medium.onnx.json"
+install_piper_voice() {
+  local voice_dir="$1"
+  local voice_file="$2"
+
+  mkdir -p "/usr/local/share/piper-voices/${voice_dir}"
+  wget -O "/usr/local/share/piper-voices/${voice_dir}/${voice_file}.onnx" \
+    "https://huggingface.co/rhasspy/piper-voices/resolve/main/${voice_dir}/${voice_file}.onnx"
+  wget -O "/usr/local/share/piper-voices/${voice_dir}/${voice_file}.onnx.json" \
+    "https://huggingface.co/rhasspy/piper-voices/resolve/main/${voice_dir}/${voice_file}.onnx.json"
+}
+
+install_piper_voice "en/en_US/lessac/medium" "en_US-lessac-medium"
+install_piper_voice "en/en_US/joe/medium" "en_US-joe-medium"
+install_piper_voice "en/en_US/ryan/medium" "en_US-ryan-medium"
 
 rm -f /tmp/piper.tar.gz
