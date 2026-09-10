e50a8d4964b0fb6eb309d258b14b14c946858b13
Eternity Ready, LLC. <73216191+eternityready2@users.noreply.github.com>
2026-09-09 23:34:09 -0500

    Remove disproven Buzzsprout User-Agent workaround

--- backend/src/Service/GuzzleFactory.php
+++ backend/src/Service/GuzzleFactory.php
@@ -6,8 +6,6 @@ namespace App\Service;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
-use GuzzleHttp\RequestOptions;
-use Psr\Http\Message\RequestInterface;
 
 final readonly class GuzzleFactory
 {
@@ -38,41 +36,6 @@ final readonly class GuzzleFactory
 
     public function buildClient(array $config = []): Client
     {
-        $mergedConfig = array_merge($this->defaultConfig, $config);
-        $handlerStack = $mergedConfig['handler'] ?? HandlerStack::create();
-
-        if ($handlerStack instanceof HandlerStack) {
-            $handlerStack = clone $handlerStack;
-            $handlerStack->push(
-                static function (callable $handler): callable {
-                    return static function (RequestInterface $request, array $options) use ($handler) {
-                        $userAgent = $request->getHeaderLine('User-Agent');
-                        $isPodcastMediaDownload = str_starts_with(
-                            $userAgent,
-                            'AzuraCast/1.0 (Podcast Import)'
-                        ) && array_key_exists(RequestOptions::SINK, $options);
-
-                        if ($isPodcastMediaDownload) {
-                            $host = strtolower($request->getUri()->getHost());
-                            $isBuzzsprout = $host === 'buzzsprout.com'
-                                || str_ends_with($host, '.buzzsprout.com');
-
-                            if ($isBuzzsprout) {
-                                $request = $request->withHeader(
-                                    'User-Agent',
-                                    'Mozilla/5.0 (compatible; AzuraCast Podcast Import/1.0; +https://www.azuracast.com/)'
-                                );
-                            }
-                        }
-
-                        return $handler($request, $options);
-                    };
-                },
-                'podcast-buzzsprout-user-agent'
-            );
-            $mergedConfig['handler'] = $handlerStack;
-        }
-
-        return new Client($mergedConfig);
+        return new Client(array_merge($this->defaultConfig, $config));
     }
 }
