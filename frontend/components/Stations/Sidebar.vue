02e227ab69aa1179e9167d6c3087d82fafdd6856
Eternity Ready, LLC. <73216191+eternityready2@users.noreply.github.com>
2026-09-12 23:33:56 -0500

    Use organized station sidebar menu

--- frontend/components/Stations/Sidebar.vue
+++ frontend/components/Stations/Sidebar.vue
@@ -53,14 +53,14 @@
 import {ref} from "vue";
 import SidebarMenu from "~/components/Common/SidebarMenu.vue";
 import {toRefs, useIntervalFn} from "@vueuse/core";
-import {useStationsMenu} from "~/components/Stations/menu";
+import {useOrganizedStationsMenu} from "~/components/Stations/useOrganizedStationsMenu.ts";
 import useStationDateTimeFormatter from "~/functions/useStationDateTimeFormatter.ts";
 import {useLuxon} from "~/vendor/luxon.ts";
 import {StationPermissions} from "~/entities/ApiInterfaces.ts";
 import {useStationData} from "~/functions/useStationQuery.ts";
 import {useUserAllowedForStation} from "~/functions/useUserallowedForStation.ts";
 
-const menuItems = useStationsMenu();
+const menuItems = useOrganizedStationsMenu();
 const {userAllowedForStation} = useUserAllowedForStation();
 
 const stationData = useStationData();
