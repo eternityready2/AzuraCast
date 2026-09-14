4359991503f969882a2e11b7bf33bed7dbe940e8
Eternity Ready, LLC. <73216191+eternityready2@users.noreply.github.com>
2026-09-13 21:33:09 -0500

    Preserve recent two-tab playlist editor UX

--- frontend/components/Stations/Playlists/EditModal.vue
+++ frontend/components/Stations/Playlists/EditModal.vue
@@ -13,7 +13,6 @@
         <tabs>
             <form-basic-info/>
             <form-schedule v-model:schedule-items="form.schedule_items" />
-            <form-advanced/>
         </tabs>
     </modal-form>
 </template>
@@ -21,7 +20,6 @@
 <script setup lang="ts">
 import FormBasicInfo from "~/components/Stations/Playlists/Form/BasicInfo.vue";
 import FormSchedule from "~/components/Stations/Playlists/Form/Schedule.vue";
-import FormAdvanced from "~/components/Stations/Playlists/Form/Advanced.vue";
 import {BaseEditModalEmits, BaseEditModalProps, useBaseEditModal} from "~/functions/useBaseEditModal";
 import {computed, toRef, useTemplateRef} from "vue";
 import {useTranslate} from "~/vendor/gettext";
